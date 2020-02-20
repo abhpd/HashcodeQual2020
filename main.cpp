2452b32d29f16c562d625127b1c6b909bf81e2d6
Abhishek Prasad <abhpdmail@gmail.com>
2020-02-20 23:45:58 +0530

    a

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fstream>
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<float> vf;
+
 
 int main()
 {
+    fstream fs;
+    fs.open("a_example.txt");
     return 0;
 }
